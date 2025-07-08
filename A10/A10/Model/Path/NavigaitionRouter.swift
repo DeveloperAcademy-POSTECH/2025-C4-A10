452fda8f9fe27d22307e87eaa0bd80f2cac73f27
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-09 00:39:26 +0900

    feat: 기본적인 Navigation Router 설정

---
+++ A10/A10/Model/Path/NavigaitionRouter.swift
@@ -0,0 +1,28 @@
+//
+//  Path.swift
+//  A10
+//
+//  Created by 김민석 on 7/9/25.
+//
+
+import Foundation
+
+class NavigaitionRouter: ObservableObject {
+    @Published var destinations: [NavigationDestination]
+    
+    init(destinations: [NavigationDestination] = []) {
+        self.destinations = destinations
+    }
+    
+    func push(to view: NavigationDestination) {
+        destinations.append(view)
+    }
+    
+    func pop() {
+        _ = destinations.popLast()
+    }
+    
+    func popToRootView() {
+        destinations = []
+    }
+}
