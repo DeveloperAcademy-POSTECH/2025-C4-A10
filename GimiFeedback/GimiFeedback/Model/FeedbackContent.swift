b80da1852ef93452db8116c8fd897bf77f15804c
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-09 12:02:59 +0900

    feat: 피드백 내용 init과 접근제어자 구현

--- GimiFeedback/GimiFeedback/Model/FeedbackContent.swift
+++ GimiFeedback/GimiFeedback/Model/FeedbackContent.swift
@@ -8,6 +8,18 @@
 struct FeedbackContent {
     let content: String
     let spicy: Int // 매운 맛 단계
-    let visiable: Bool
+    var visiable: Bool
     let type: FeedbackContentType
+    
+    init(
+        content: String,
+        spicy: Int,
+        visiable: Bool = false,
+        type: FeedbackContentType
+    ) {
+        self.content = content
+        self.spicy = spicy
+        self.visiable = visiable
+        self.type = type
+    }
 }
