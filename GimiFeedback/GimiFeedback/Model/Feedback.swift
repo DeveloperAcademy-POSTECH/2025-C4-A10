5413c22b2c0ad33e2cf93e6e92a37da0fc694126
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-09 12:01:28 +0900

    feat: 피드백 init과 접근제어자 구현

--- GimiFeedback/GimiFeedback/Model/Feedback.swift
+++ GimiFeedback/GimiFeedback/Model/Feedback.swift
@@ -11,8 +11,26 @@ struct Feedback {
     let id: UUID
     let read: UUID  // 받는 사람
     let write: String   // 작성한 사람 닉네임
-    let content: [FeedbackContent]
+    var content: [FeedbackContent]
     let date: Date
     let title: String   // Category의 title
     var visiable: Bool
+    
+    init(
+        id: UUID = UUID(),
+        read: UUID,
+        write: String,
+        content: [FeedbackContent],
+        date: Date = Date(),
+        title: String,
+        visiable: Bool = false
+    ) {
+        self.id = id
+        self.read = read
+        self.write = write
+        self.content = content
+        self.date = date
+        self.title = title
+        self.visiable = visiable
+    }
 }
