d6fa55aa4a567a5ccd5423df9275756602bd4450
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-12 17:47:54 +0900

    feat: Kakao SDK 앱 시작점에 추가

--- GimiFeedback/GimiFeedback/App/GimiFeedbackApp.swift
+++ GimiFeedback/GimiFeedback/App/GimiFeedbackApp.swift
@@ -7,14 +7,25 @@
 
 import SwiftUI
 import FirebaseCore
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct GimiFeedbackApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
+    init() {
+        KakaoSDK.initSDK(appKey: Bundle.kakaoNativeAppKey)
+    }
+    
     var body: some Scene {
         WindowGroup {
             OnboardingStartView()
+                .onOpenURL { url in
+                    if AuthApi.isKakaoTalkLoginUrl(url) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                }
         }
     }
 }
