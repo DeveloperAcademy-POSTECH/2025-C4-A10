7fedd603e8a53a1d487e04f9912705e9ae3511be
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-12 16:56:26 +0900

    코드 입력 및 로그인 화면 이동 구현(#15) (#51)
    * feat: 기본적으로 필요한 파일 구현
    
    * feat: 기본적으로 필요한 UI 생성
    
    * feat: 처음 분기에 따른 화면 Navigation 설정
    
    * feat: 온보딩 화면 분기에서 각 화면 이동 처리 구현
    
    * feat: Navigation을 typealias를 통해 간소화
    
    * feat: NavigationDestination을 protocol을 이용해서 채택하도록 구현

---
+++ GimiFeedback/GimiFeedback/Model/Navigation/NavigationRouter.swift
@@ -0,0 +1,31 @@
+//
+//  NavigationRouter.swift
+//  GimiFeedback
+//
+//  Created by 김민석 on 7/9/25.
+//
+
+import Foundation
+
+final class NavigationRouter<T: NavigationDestination>: ObservableObject {
+    @Published var destinations: [T]
+    
+    init(destinations: [T] = []) {
+        self.destinations = destinations
+    }
+    
+    func push(to view: T) {
+        destinations.append(view)
+    }
+    
+    func pop() {
+        _ = destinations.popLast()
+    }
+    
+    func popToRootView() {
+        destinations = []
+    }
+}
+
+typealias OnboardingNavigationRouter = NavigationRouter<OnboardingNavigationDestination>
+typealias MainNavigationRouter = NavigationRouter<MainNavigationDestination>
---
+++ GimiFeedback/GimiFeedback/Presentation/Onboarding/OnboardingStartView.swift
@@ -0,0 +1,67 @@
+//
+//  OnboardingStartView.swift
+//  GimiFeedback
+//
+//  Created by 김민석 on 7/8/25.
+//
+
+import SwiftUI
+
+struct OnboardingStartView: View {
+    @StateObject var router: OnboardingNavigationRouter
+    
+    init() {
+        _router = StateObject(wrappedValue: OnboardingNavigationRouter())
+    }
+    
+    var body: some View {
+        NavigationStack(path: $router.destinations) {
+            ContentView()
+                .navigationDestination(for: OnboardingNavigationDestination.self) { destination in
+                    switch destination {
+                    case .inputCode:
+                        InputCodeView()
+                    case .login:
+                        OnboardingLoginView()
+                    case .feedbackWriteComplete:
+                        // TODO: 피드백 완료 이동
+                        EmptyView()
+                    case .feedbackWrite:
+                        // TODO: 피드백 생성 이동
+                        EmptyView()
+                    }
+                }
+        }
+        .environmentObject(router)
+        
+    }
+}
+
+extension OnboardingStartView {
+    private struct ContentView: View {
+        @EnvironmentObject var router: OnboardingNavigationRouter
+        
+        var body: some View {
+            VStack {
+                Text("솔직한 피드백, 더 나은 나를 위한 성장")
+                Text("얼마나 매운 맛일지 미리 맛봐드릴게요. 상처는 적게, 성장은 크게.")
+                
+                Button {
+                    router.push(to: .inputCode)
+                } label: {
+                    Text("코드 입력하기")
+                }
+                
+                Button {
+                    router.push(to: .login)
+                } label: {
+                    Text("로그인")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    OnboardingStartView()
+}
