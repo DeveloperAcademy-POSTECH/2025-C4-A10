47f41cfb716db7be36cbe62d2ded14fbbb5dcf09
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-13 17:03:01 +0900

    feat: 파이어베이스 로그인 회원가입 로직 동작 구현

--- GimiFeedback/GimiFeedback/Presentation/Onboarding/LoginViewModel.swift
+++ GimiFeedback/GimiFeedback/Presentation/Onboarding/LoginViewModel.swift
@@ -5,7 +5,6 @@
 //  Created by 김민석 on 7/12/25.
 //
 
-@MainActor
 final class LoginViewModel: ViewModelable {
     enum Action {
         case kakaoLogin
@@ -15,23 +14,69 @@ final class LoginViewModel: ViewModelable {
     func send(_ action: Action) {
         switch action {
         case .kakaoLogin:
-            kakaoLogin()
+            Task {
+                if let result = await kakaoLogin() {
+                    await firebaseAuthSignUpOrLogin(
+                        email: result.email,
+                        password: result.password
+                    )
+                }
+            }
         case .kakaoLogout:
             kakaoLogout()
         }
     }
-    
-    private func kakaoLogin() {
-        Task {
-            do {
-                let (email, password) = try await KakaoAuthManager.shared.kakaoAuthSignIn()
-                print("로그인 성공: \(email), 비밀번호: \(password)")
-            } catch {
-                print("로그인 실패: \(error.localizedDescription)")
-            }
+}
+
+// MARK: - Private 로직 함수
+
+extension LoginViewModel {
+    /// 카카오 로그인
+    private func kakaoLogin() async -> (email: String, password: String)? {
+        do {
+            let result = try await KakaoAuthManager.shared.kakaoAuthSignIn()
+            print("카카오 로그인 성공: \(result.email)")
+            
+            return result
+        } catch {
+            print("카카오 로그인 실패: \(error.localizedDescription)")
+            
+            return nil
         }
     }
     
+    /// 파이어베이스 회원가입 또는 로그인
+    private func firebaseAuthSignUpOrLogin(email: String, password: String) async {
+        do {
+            let result = try await FirebaseAuthManager.shared.emailAuthSignUp(
+                email: email,
+                password: password
+            )
+            
+            print("파이어베이스 회원가입 성공: \(result)")
+        } catch {
+            if FirebaseAuthManager.shared.isEmailAlreadyInUseError(error) {
+                await firebaseAuthLogin(email: email, password: password)
+            } else {
+                print("회원가입 실패: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    /// 파이어베이스 로그인 로직
+    private func firebaseAuthLogin(email: String, password: String) async {
+        do {
+            let result = try await FirebaseAuthManager.shared.emailAuthLogin(
+                email: email,
+                password: password
+            )
+            print("파이어베이스 로그인 성공: \(result)")
+        } catch {
+            print("로그인 실패: \(error.localizedDescription)")
+        }
+    }
+    
+    /// 카카오 로그아웃 로직
     private func kakaoLogout() {
         Task {
             do {
