3e72a64c5a8726a066530de3bd37cbfca2169eae
승찬 <anstmdcks12@naver.com>
2025-07-13 22:55:01 +0900

    chore: formattedDate 뷰에서 분리

--- GimiFeedback/GimiFeedback/Presentation/FeedbackChannel/FeedbackChannelView.swift
+++ GimiFeedback/GimiFeedback/Presentation/FeedbackChannel/FeedbackChannelView.swift
@@ -104,12 +104,3 @@ struct FeedbackChannelView: View {
         
     }
 }
-
-extension Date {
-    fileprivate var formattedDate: String {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "ko_KR")
-        formatter.dateFormat = "yyyy년 M월 d일"
-        return formatter.string(from: self)
-    }
-}
