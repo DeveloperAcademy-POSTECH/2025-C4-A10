45eed2c259ac960112a417ca7933e68d53f2da8f
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-13 17:02:12 +0900

    feat: 파이어베이스 회원가입 및 로그인 로직 구현

--- GimiFeedback/GimiFeedback/Network/FirebaseAuthManager.swift
+++ GimiFeedback/GimiFeedback/Network/FirebaseAuthManager.swift
@@ -13,9 +13,26 @@ final class FirebaseAuthManager {
     
     private init() {}
     
-    func emailAuthSignUp(email: String, password: String) async throws {
+    /// 회원가입
+    func emailAuthSignUp(email: String, password: String) async throws -> String {
         let result = try await Auth.auth().createUser(withEmail: email, password: password)
         
-        print(result)
+        return result.user.email ?? ""
+    }
+
+    /// 로그인
+    func emailAuthLogin(email: String, password: String) async throws -> String {
+        let result = try await Auth.auth().signIn(withEmail: email, password: password)
+        
+        return result.user.email ?? ""
+    }
+
+    /// 이미 회원가입 된 유저 처리
+    func isEmailAlreadyInUseError(_ error: Error) -> Bool {
+        if let errCode = AuthErrorCode(rawValue: (error as NSError).code),
+           errCode == .emailAlreadyInUse {
+            return true
+        }
+        return false
     }
 }
