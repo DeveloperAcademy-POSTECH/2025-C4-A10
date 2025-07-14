8a24f26d9a5824a0c5db1006f49e460b503b9f3a
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2025-07-14 16:30:23 +0900

    피드백 채널 리스트 View(홈) 구현 (#38) (#52)
    * feat: 채널리스트 불러오기 및 해당 피드백 갯수 표현
    
    * feat: 선택된 채널로 이동 및 속해있는 피드백들 불러오기
    
    * feat: 전체 피드백 개수 추가 및 UI 수정
    
    * feat: 피드백 삭제 로직 및 UI 수정
    
    * chore: formattedDate 뷰에서 분리
    
    * chore: 필요없는 로직 삭제
    
    * style: SwiftLint 조건 추가
    
    * refactor: 피드백 삭제 방식 수정
    
    * refactor: 데이터 관리 방법 수정

--- GimiFeedback/GimiFeedback/Presentation/FeedbackList/FeedbackListViewModel.swift
+++ GimiFeedback/GimiFeedback/Presentation/FeedbackList/FeedbackListViewModel.swift
@@ -1,24 +1,78 @@
-//
-//  FeedbackListViewModel.swift
-//  GimiFeedback
-//
-//  Created by 김민석 on 7/8/25.
-//
-
 import Foundation
 
 final class FeedbackListViewModel: ViewModelable {
     
     enum Action {
-        case load
+        case fetchFeedbackChannelList
+        case clearError
     }
     
-    @Published private(set) var feedbacks: [String] = []
+    @Published private(set) var feedbackChannelList: [FeedbackChannelInfo] = []
+    @Published private(set) var totalFeedbackCount: Int = .zero
+    
+    @Published private(set) var errorMessage: String?
+    @Published private(set) var isFeedbackChannelListLoading: Bool = false
     
     func send(_ action: Action) {
         switch action {
-        case .load:
-            print("hello")
+        case .fetchFeedbackChannelList:
+            fetchFeedbackChannelList()
+            
+        case .clearError:
+            errorMessage = nil
         }
+    }
+}
+
+extension FeedbackListViewModel {
+    private func fetchFeedbackChannelList(){
+        Task {
+            isFeedbackChannelListLoading = true
+            
+            totalFeedbackCount = .zero
+            
+            do {
+                let channelList = try await FirestoreManager.shared.fetch(
+                    as: FeedbackChannel.self,
+                    .feedbackChannel
+                )
+                
+                var result: [FeedbackChannelInfo] = []
+                
+                for channel in channelList {
+                    let feedbackList = try await FirestoreManager.shared.fetch(
+                        as: Feedback.self,
+                        .feedback,
+                        whereFeild: "feedbackChannelID",
+                        equalData: channel.id.uuidString
+                    )
+                    
+                    result.append(
+                        FeedbackChannelInfo(
+                            channel: channel,
+                            feedbackCount: feedbackList.count,
+                            visibleFeedbackCount: feedbackList.filter({ !$0.visiable }).count)
+                    )
+                    
+                    totalFeedbackCount += feedbackList.count
+                }
+                
+                feedbackChannelList = result
+                
+            } catch {
+                errorMessage = error.localizedDescription
+            }
+            isFeedbackChannelListLoading = false
+        }
+    }
+}
+
+struct FeedbackChannelInfo: Identifiable {
+    let channel: FeedbackChannel
+    let feedbackCount: Int
+    let visibleFeedbackCount: Int
+    
+    var id: UUID {
+        channel.id
     }
 }
---
+++ GimiFeedback/GimiFeedback/Util/Extension/Date+Extension.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension Date {
+    var formattedDate: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy년 M월 d일"
+        return formatter.string(from: self)
+    }
+}
+
