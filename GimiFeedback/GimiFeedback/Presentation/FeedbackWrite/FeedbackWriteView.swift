0aea5a53d42f6cc6c8d69ee4ece73d31d4028c55
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-15 15:56:45 +0900

    refactor: 임시 View들 기본 생성

--- GimiFeedback/GimiFeedback/Presentation/FeedbackWrite/FeedbackWriteView.swift
+++ GimiFeedback/GimiFeedback/Presentation/FeedbackWrite/FeedbackWriteView.swift
@@ -1,1 +1,18 @@
-import Foundation
+//
+//  FeedbackWriteView.swift
+//  GimiFeedback
+//
+//  Created by 김민석 on 7/15/25.
+//
+
+import SwiftUI
+
+struct FeedbackWriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedbackWriteView()
+}
