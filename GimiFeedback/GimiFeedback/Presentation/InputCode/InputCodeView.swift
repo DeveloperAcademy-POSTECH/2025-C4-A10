1c7c6ab45fbca9c4b7db7d2bc45652226e7cd0d4
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-16 22:54:39 +0900

    feat: 코드 입력 뷰에서 코드 입력하고 검증하는 버튼 추가

--- GimiFeedback/GimiFeedback/Presentation/InputCode/InputCodeView.swift
+++ GimiFeedback/GimiFeedback/Presentation/InputCode/InputCodeView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct InputCodeView: View {
+    @StateObject var viewModel: InputCodeViewModel
+    
+    init() {
+        _viewModel = StateObject(wrappedValue: .init())
+    }
+    
     var body: some View {
-        Text("Input Code View")
+        VStack(alignment: .leading) {
+            Text("코드 입력하기")
+            
+            TextField("받은 코드를 붙여 넣어주세요", text: $viewModel.code)
+                .padding(.vertical, 8)
+                .overlay(
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(.gray),
+                    alignment: .bottom
+                )
+            
+            Spacer()
+            
+            Button(action: {
+                viewModel.send(.verifyCode)
+            }, label: {
+                Text("완료하기")
+                    .font(Font.system(size: 20, weight: .bold))
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, maxHeight: 66)
+                    .background(Color.black)
+                    .cornerRadius(20)
+            })
+            .disabled(viewModel.code.isEmpty)
+        }.padding()
     }
 }
 
