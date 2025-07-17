271f0b4390dd9001615c4463bf0f1b6319ad14be
UnkyungJo <pos10022@unist.ac.kr>
2025-07-17 23:15:38 +0900

    feat: 문자열 대신 enum으로 수정

--- GimiFeedback/GimiFeedback/Presentation/FeedbackDetail/SubView/FeedbackSectionView.swift
+++ GimiFeedback/GimiFeedback/Presentation/FeedbackDetail/SubView/FeedbackSectionView.swift
@@ -8,7 +8,19 @@
 import SwiftUI
 
 struct FeedbackSectionView: View {
-    let title: String
+    let type: FeedbackContentType
+    var title: String {
+        switch type {
+        case .keep:
+            return "Keep"
+        case .problem:
+            return "Problem"
+        case .try:
+            return "Try"
+        case .other:
+            return "Other"
+        }
+    }
     @Binding var details: [FeedbackContent]
     let onReveal: (FeedbackContent) -> Void
     
@@ -49,6 +61,63 @@ struct FeedbackSectionView: View {
     }
 }
 
-//#Preview {
-//    FeedbackSectionView()
-//}
+#Preview {
+    var sampleFeedbackContents: [FeedbackContent] = [
+        FeedbackContent(
+            content: "사용자 온보딩 플로우가 명확해서 이해하기 쉬웠어요.",
+            spicy: 1,
+            visiable: false,
+            type: .keep
+        ),
+        FeedbackContent(
+            content: "피드 로딩 시간이 너무 길어져서 기다리기 힘들어요.",
+            spicy: 4,
+            visiable: false,
+            type: .problem
+        ),
+        FeedbackContent(
+            content: "다양한 로그인 옵션을 제공하면 접근성이 더 좋아질 것 같아요.",
+            spicy: 2,
+            visiable: false,
+            type: .try
+        ),
+        FeedbackContent(
+            content: "앱 테마 색상을 계절에 따라 바꾸는 것도 재미있을 것 같아요.",
+            spicy: 2,
+            visiable: false,
+            type: .other
+        ),
+        FeedbackContent(
+            content: "고객센터 응답이 빨라서 좋았습니다. 감사합니다.",
+            spicy: 1,
+            visiable: false,
+            type: .keep
+        ),
+        FeedbackContent(
+            content: "알림 설정이 복잡해서 원하는 항목만 선택하기 어려웠어요.",
+            spicy: 3,
+            visiable: false,
+            type: .problem
+        ),
+        FeedbackContent(
+            content: "이벤트 기능을 시도해보면 사용자 참여율이 높아질 것 같아요.",
+            spicy: 3,
+            visiable: false,
+            type: .try
+        ),
+        FeedbackContent(
+            content: "툴팁에 이모지를 넣으면 더 직관적으로 느껴질 수 있을 듯해요.",
+            spicy: 1,
+            visiable: false,
+            type: .try
+        )
+    ]
+    
+    @State var feedbacks = sampleFeedbackContents
+    
+    FeedbackSectionView(type: .keep, details: $feedbacks, onReveal: { detail in
+        if let index = feedbacks.firstIndex(where: { $0.id == detail.id }) {
+            feedbacks[index].visiable = true
+        }
+    })
+}
