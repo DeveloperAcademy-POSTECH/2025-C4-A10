4852f7fca852b5ac6078b9e89689159164406677
UnkyungJo <pos10022@unist.ac.kr>
2025-07-17 16:26:15 +0900

    refactor: 임시데이터 넣는 부분 삭제

--- GimiFeedback/GimiFeedback/Presentation/FeedbackDetail/FeedbackDetailView.swift
+++ GimiFeedback/GimiFeedback/Presentation/FeedbackDetail/FeedbackDetailView.swift
@@ -67,9 +67,6 @@ struct FeedbackDetailView: View {
                     }
                 }
             }
-            .onAppear {
-                viewModel.send(.createDummyDetails)
-            }
         }
     }
 }
