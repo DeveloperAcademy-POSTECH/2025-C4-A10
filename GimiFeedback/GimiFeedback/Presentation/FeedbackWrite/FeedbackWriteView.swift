61aae0e586a0eec093124bef5a4c9305daffa763
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-17 12:01:38 +0900

    feat: 생성 버튼 구현 및 UI 구성

--- GimiFeedback/GimiFeedback/Presentation/FeedbackWrite/FeedbackWriteView.swift
+++ GimiFeedback/GimiFeedback/Presentation/FeedbackWrite/FeedbackWriteView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct FeedbackWriteView: View {
     @StateObject var viewModel: FeedbackWriteViewModel
+    @State private var isShowCreateAlert: Bool = false
     
     init(feedbackChannel: FeedbackChannel) {
         _viewModel = StateObject(
@@ -20,24 +21,47 @@ struct FeedbackWriteView: View {
     
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 16) {
-                Text(viewModel.feedbackChannel.channelTitle)
-                Text(viewModel.feedbackChannel.content)
+            VStack(alignment: .leading, spacing: 40) {
+                VStack {
+                    Text(viewModel.feedbackChannel.channelTitle)
+                    Text(viewModel.feedbackChannel.content)
+                }
                 
-                Text("닉네임")
-                Text("받는 사람에게 보여지는 닉네임입니다.")
-                TextField("", text: $viewModel.nickName)
-                    .padding()
-                    .background(Color(UIColor.systemGray6))
-                    .cornerRadius(5)
+                Rectangle()
+                    .fill(Color.gray)
+                    .frame(height: 8)
+                    .frame(maxWidth: .infinity)
+                    .padding(-20)
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("닉네임")
+                    Text("받는 사람에게 보여지는 닉네임입니다.")
+                    TextField("", text: $viewModel.nickName)
+                        .padding()
+                        .background(Color(UIColor.systemGray6))
+                        .cornerRadius(5)
+                }
                 
                 FeedbackContentView(content: $viewModel.keeps, contentType: .keep)
                 FeedbackContentView(content: $viewModel.problems, contentType: .problem)
                 FeedbackContentView(content: $viewModel.trys, contentType: .try)
                 FeedbackContentView(content: $viewModel.others, contentType: .other)
+                
+                Button(action: {
+                    isShowCreateAlert = true
+                }, label: {
+                    Text("완료하기")
+                        .font(Font.system(size: 20, weight: .bold))
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 64)
+                        .background(viewModel.canCreate ? Color.black : Color.gray.opacity(0.5))
+                        .cornerRadius(16)
+                })
+                .disabled(viewModel.canCreate)
             }
         }
-        .padding()
+        .contentMargins(.horizontal, 20, for: .scrollContent)
         .navigationTitle("피드백 작성하기")
         .navigationBarTitleDisplayMode(.inline)
     }
@@ -50,28 +74,30 @@ extension FeedbackWriteView {
         var contentType: FeedbackContentType
         
         var body: some View {
-            Text(contentType.title)
-            Text(contentType.content)
-            ForEach(content.indices, id: \.self) { index in
-                TextField("", text: $content[index])
-                    .frame(height: 102)
-                    .padding()
-                    .background(Color(UIColor.systemGray6))
-                    .cornerRadius(5)
-            }
-            
-            if content.count < 3 {
-                Button {
-                    content.append("")
-                } label: {
-                    Text("+ 항목 추가하기")
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 56)
-                        .foregroundColor(.black)
-                        .background(Color.gray.opacity(0.3))
-                        .cornerRadius(12)
+            VStack(alignment: .leading, spacing: 10) {
+                Text(contentType.title)
+                Text(contentType.content)
+                ForEach(content.indices, id: \.self) { index in
+                    TextField("", text: $content[index])
+                        .frame(height: 102)
+                        .padding()
+                        .background(Color(UIColor.systemGray6))
+                        .cornerRadius(5)
                 }
-                .padding(.top, 8)
+                
+                if content.count < 3 {
+                    Button {
+                        content.append("")
+                    } label: {
+                        Text("+ 항목 추가하기")
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 56)
+                            .foregroundColor(.black)
+                            .background(Color.gray.opacity(0.3))
+                            .cornerRadius(12)
+                    }
+                    .padding(.top, 8)
+                }
             }
         }
     }
