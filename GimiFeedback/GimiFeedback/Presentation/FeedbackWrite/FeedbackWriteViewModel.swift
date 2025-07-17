dcba61eed93b5466b12b11337c09ec9e3ae2cb7d
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-17 11:27:55 +0900

    feat: ViewModel에 필요한 데이터 추가

--- GimiFeedback/GimiFeedback/Presentation/FeedbackWrite/FeedbackWriteViewModel.swift
+++ GimiFeedback/GimiFeedback/Presentation/FeedbackWrite/FeedbackWriteViewModel.swift
@@ -5,12 +5,19 @@
 //  Created by 김민석 on 7/16/25.
 //
 
+import Foundation
+
 final class FeedbackWriteViewModel: ViewModelable {
     enum Action {
         case feedbackWrite
     }
     
     let feedbackChannel: FeedbackChannel
+    @Published var nickName: String = ""
+    @Published var keeps: [String] = [""]
+    @Published var problems: [String] = [""]
+    @Published var trys: [String] = [""]
+    @Published var others: [String] = [""]
     
     init(feedbackChannel: FeedbackChannel) {
         self.feedbackChannel = feedbackChannel
