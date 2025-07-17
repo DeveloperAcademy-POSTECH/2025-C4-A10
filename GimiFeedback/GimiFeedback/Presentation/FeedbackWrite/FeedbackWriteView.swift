5e1ff559dc4e313e1366418bb315e11bf3b4da1f
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-17 11:28:30 +0900

    feat: 기본적으로 필요한 UI 컴포넌트들 생성

--- GimiFeedback/GimiFeedback/Presentation/FeedbackWrite/FeedbackWriteView.swift
+++ GimiFeedback/GimiFeedback/Presentation/FeedbackWrite/FeedbackWriteView.swift
@@ -19,8 +19,56 @@ struct FeedbackWriteView: View {
     }
     
     var body: some View {
-        Text(viewModel.feedbackChannel.channelTitle)
-        Text(viewModel.feedbackChannel.content)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) {
+                Text(viewModel.feedbackChannel.channelTitle)
+                Text(viewModel.feedbackChannel.content)
+                
+                Text("닉네임")
+                Text("받는 사람에게 보여지는 닉네임입니다.")
+                TextField("", text: $viewModel.nickName)
+                    .padding()
+                    .background(Color(UIColor.systemGray6))
+                    .cornerRadius(5)
+                
+                FeedbackContentView(content: $viewModel.keeps, contentType: .keep)
+                FeedbackContentView(content: $viewModel.problems, contentType: .problem)
+                FeedbackContentView(content: $viewModel.trys, contentType: .try)
+                FeedbackContentView(content: $viewModel.others, contentType: .other)
+            }
+        }
+        .padding()
+        .navigationTitle("피드백 작성하기")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+extension FeedbackWriteView {
+    struct FeedbackContentView: View {
+        @Binding var content: [String]
+        
+        var contentType: FeedbackContentType
+        
+        var body: some View {
+            Text(contentType.title)
+            Text(contentType.content)
+            TextField("", text: $content[0])
+                .frame(height: 102)
+                .padding()
+                .background(Color(UIColor.systemGray6))
+                .cornerRadius(5)
+            
+            Button {
+                // TODO: 항목 추가하기 버튼 동작
+            } label: {
+                Text("+ 항목 추가하기")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 56)
+                    .foregroundColor(.black)
+                    .background(.gray)
+                    .cornerRadius(12)
+            }
+        }
     }
 }
 
