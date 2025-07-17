0fe6988414e811eae6a3c18b55836a82d214c63a
UnkyungJo <pos10022@unist.ac.kr>
2025-07-17 16:49:47 +0900

    refactor: NabigationStack 제거

--- GimiFeedback/GimiFeedback/Presentation/FeedbackDetail/FeedbackDetailView.swift
+++ GimiFeedback/GimiFeedback/Presentation/FeedbackDetail/FeedbackDetailView.swift
@@ -10,63 +10,62 @@ struct FeedbackDetailView: View {
     }
     
     var body: some View {
-        NavigationStack {
-            VStack(alignment: .leading, spacing: 16) {
-                Text("\(viewModel.feedbackItem.date.formattedDate)")
-                
-                VStack(spacing: 8) {
-                    FeedbackSectionView(
-                        title: "Keep",
-                        details: $viewModel.keepFeedback,
-                        onReveal: { detail in
-                            viewModel.send(.visualizeDetail(detail: detail))
-                        }
-                    )
-                    FeedbackSectionView(
-                        title: "Problem",
-                        details: $viewModel.problemFeedback,
-                        onReveal: { detail in
-                            viewModel.send(.visualizeDetail(detail: detail))
-                        }
-                    )
-                    FeedbackSectionView(
-                        title: "try",
-                        details: $viewModel.tryFeedback,
-                        onReveal: { detail in
-                            viewModel.send(.visualizeDetail(detail: detail))
-                        }
-                    )
+        VStack(alignment: .leading, spacing: 16) {
+            Text("\(viewModel.feedbackItem.date.formattedDate)")
+            
+            VStack(spacing: 8) {
+                FeedbackSectionView(
+                    title: "Keep",
+                    details: $viewModel.keepFeedback,
+                    onReveal: { detail in
+                        viewModel.send(.visualizeDetail(detail: detail))
+                    }
+                )
+                FeedbackSectionView(
+                    title: "Problem",
+                    details: $viewModel.problemFeedback,
+                    onReveal: { detail in
+                        viewModel.send(.visualizeDetail(detail: detail))
+                    }
+                )
+                FeedbackSectionView(
+                    title: "try",
+                    details: $viewModel.tryFeedback,
+                    onReveal: { detail in
+                        viewModel.send(.visualizeDetail(detail: detail))
+                    }
+                )
+            }
+        }
+        .alert("피드백 삭제하기", isPresented: $showDeleteAlert) {
+            Button("취소", role: .cancel) {}
+            Button("확인", role: .destructive) {
+                viewModel.send(.deleteFeedback)
+            }
+        } message: {
+            Text("정말 삭제하시겠습니까?\n이 작업은 되돌릴 수 없습니다.")
+        }
+        .navigationTitle("\(viewModel.feedbackItem.writePerson)의 피드백")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button(action: {
+                    // TODO: 뒤로가기
+                }) {
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(.black)
                 }
             }
-            .alert("피드백 삭제하기", isPresented: $showDeleteAlert) {
-                Button("취소", role: .cancel) {}
-                Button("확인", role: .destructive) {
-                    viewModel.send(.deleteFeedback)
-                }
-            } message: {
-                Text("정말 삭제하시겠습니까?\n이 작업은 되돌릴 수 없습니다.")
-            }
-            .navigationTitle("\(viewModel.feedbackItem.writePerson)의 피드백")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button(action: {
-                        // TODO: 뒤로가기
-                    }) {
-                        Image(systemName: "chevron.left")
-                            .foregroundColor(.black)
-                    }
-                }
-                
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button(action: {
-                        showDeleteAlert = true
-                    }) {
-                        Image(systemName: "trash")
-                            .foregroundColor(.black)
-                    }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button(action: {
+                    showDeleteAlert = true
+                }) {
+                    Image(systemName: "trash")
+                        .foregroundColor(.black)
                 }
             }
         }
+
     }
 }
