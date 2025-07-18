57dd80cf6911c7a331f5287b813451fd3a18e3fb
Ssunbell <yousun9591@naver.com>
2025-07-18 17:26:28 +0900

    feat: model

---
+++ ai/model/sequence_classification.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+from transformers import PreTrainedModel, AutoModel, AutoConfig
+
+
+class ModelForSequenceClassification(PreTrainedModel):
+    def __init__(self, config: AutoConfig, labels: list[str]) -> None:
+        super().__init__(config)
+        self.num_labels = len(labels)
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.1), nn.Linear(config.hidden_size, self.num_labels)
+        )
+        self.post_init()
+        self.model = AutoModel.from_pretrained(config._name_or_path)
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        labels=None,
+    ):
+        output = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+        )
+
+        logits = self.classifier(output[0])
+
+        loss = None
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+
+        return (loss, logits) if loss is not None else (logits,)
