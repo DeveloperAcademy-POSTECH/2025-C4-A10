696b5dd0533089e993d6f3e6f83e15de293bf358
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-21 00:32:15 +0900

    fix: MainViewModel과 알림 ViewModel 을 합쳐서 구현

--- GimiFeedback/GimiFeedback/Presentation/Main/MainView.swift
+++ GimiFeedback/GimiFeedback/Presentation/Main/MainView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct MainView: View {
     @StateObject var router: MainNavigationRouter
+    @StateObject private var viewModel: MainViewModel
     @ObservedObject var notificationRouter: NotificationViewModel
     
     init(notificationRouter: NotificationViewModel) {
         _router = StateObject(wrappedValue: MainNavigationRouter())
+        _viewModel = StateObject(wrappedValue: .init())
         self.notificationRouter = notificationRouter
     }
     
@@ -22,17 +24,26 @@ struct MainView: View {
                 .navigationDestination(for: MainNavigationDestination.self) { destination in
                     MainNavigationRoutingView(destination: destination)
                 }
+                .onOpenURL { url in
+                    viewModel.send(.handleDeepLink(url))
+                }
+                .onChange(of: viewModel.channel) { _, newValue in
+                    if let channel = newValue {
+                        router.push(to: .channelDetail(channelItem: channel))
+                        viewModel.send(.resetChannel)
+                    }
+                }
+                .onAppear {
+                    notificationRouter.send(.fetchFeedback)
+                }
+                .onChange(of: notificationRouter.feedback) { _, newValue in
+                    if let feedback = newValue {
+                        router.push(to: .feedbackDetail(feedback: feedback))
+                        notificationRouter.send(.resetFeedback)
+                    }
+                }
         }
         .environmentObject(router)
-        .onAppear {
-            notificationRouter.send(.fetchFeedback)
-        }
-        .onChange(of: notificationRouter.feedback) { _, newValue in
-            if let feedback = newValue {
-                router.push(to: .feedbackDetail(feedback: feedback))
-                notificationRouter.send(.resetFeedback)
-            }
-        }
     }
 }
 
