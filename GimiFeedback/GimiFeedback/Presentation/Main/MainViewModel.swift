3a8fa35d6047057d18509a5087cfc814612ca5e5
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-21 00:31:46 +0900

    fix: MainViewModel 파일 이동

---
+++ GimiFeedback/GimiFeedback/Presentation/Main/MainViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewModel.swift
+//  GimiFeedback
+//
+//  Created by 승찬 on 7/18/25.
+//
+
+import Foundation
+
+final class MainViewModel: ViewModelable {
+    enum Action {
+        case handleDeepLink(URL)
+        case resetChannel
+    }
+    
+    @Published private(set) var channel: FeedbackChannel? = nil
+    
+    func send(_ action: Action) {
+        switch action {
+        case .handleDeepLink(let url):
+            Task {
+                if let getChannel = await DeepLinkManager.shared.handleFeedbackWriteDeepLink(
+                    url: url
+                ) {
+                    channel = getChannel
+                }
+            }
+        case .resetChannel:
+            channel = nil
+        }
+    }
+}
