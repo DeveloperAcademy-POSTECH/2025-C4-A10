f1f990a29bc240a14dc1ae9d52db74eb31d14b00
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-22 00:52:36 +0900

    feat: 말풍선 꼬리 부분이 위로 갈 수 있도록 구현, 컴포넌트 배치

--- GimiFeedback/GimiFeedback/Presentation/Components/SpeechView.swift
+++ GimiFeedback/GimiFeedback/Presentation/Components/SpeechView.swift
@@ -9,52 +9,85 @@ import SwiftUI
 
 struct SpeechBubbleView: View {
     let message: String
-    
+    var isTailTop: Bool = false
+
     @State private var bubbleWidth: CGFloat = 0
-    
+
     var body: some View {
         VStack(spacing: 0) {
-            // Text 부분
+            if isTailTop {
+                TriangleTailView(width: bubbleWidth, isTop: true)
+            }
+
+            MessageBodyView(message: message, bubbleWidth: $bubbleWidth)
+
+            if !isTailTop {
+                TriangleTailView(width: bubbleWidth, isTop: false)
+            }
+        }
+    }
+}
+
+extension SpeechBubbleView {
+    
+    // MARK: 메시지 뷰
+    
+    struct MessageBodyView: View {
+        let message: String
+        @Binding var bubbleWidth: CGFloat
+
+        var body: some View {
             VStack(spacing: 0) {
                 Text(message)
                     .foregroundColor(.brandGreen100)
                     .lineSpacing(5)
                     .padding(16)
-                    .background(GeometryReader { geometry in
-                        Color.clear
-                            .onAppear {
-                                bubbleWidth = geometry.size.width
-                            }
-                            .onChange(of: geometry.size.width) { _, newValue in
-                                bubbleWidth = newValue
-                            }
-                    })
+                    .background(
+                        GeometryReader { geometry in
+                            Color.clear
+                                .onAppear {
+                                    bubbleWidth = geometry.size.width
+                                }
+                                .onChange(of: geometry.size.width) { _, newValue in
+                                    bubbleWidth = newValue
+                                }
+                        }
+                    )
             }
             .background(Color.primaryDarken100)
             .cornerRadius(12)
             .padding(.horizontal, 24)
-            
-            // 꼬리 부분
+        }
+    }
+    
+    // MARK: 삼각형 뷰
+    
+    struct TriangleTailView: View {
+        let width: CGFloat
+        let isTop: Bool
+
+        var body: some View {
             Triangle()
                 .fill(.primaryDarken100)
                 .frame(width: 18, height: 12)
-                .offset(x: bubbleWidth / 4, y: -2) // 메시지 크기에 따른 변경
+                .rotationEffect(.degrees(isTop ? 180 : 0))
+                .offset(x: width / 2 - 24 - 9, y: isTop ? 2 : -2)
         }
     }
-}
-
-// MARK: - Triangle
-
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
-        
-        return path
+    
+    // MARK: 삼각형 그리는 부분
+    
+    struct Triangle: Shape {
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            
+            return path
+        }
     }
 }
 
@@ -69,6 +102,10 @@ struct Triangle: Shape {
             .background(Color.gray.opacity(0.2))
         
         SpeechBubbleView(message: "안녕하세요 테스트입니다.\n안녕하세요 테스트입니다.")
+            .padding()
+            .background(Color.gray.opacity(0.2))
+        
+        SpeechBubbleView(message: "여기서 채널을 생성할 수 있어요.",isTailTop: true)
             .padding()
             .background(Color.gray.opacity(0.2))
     }
