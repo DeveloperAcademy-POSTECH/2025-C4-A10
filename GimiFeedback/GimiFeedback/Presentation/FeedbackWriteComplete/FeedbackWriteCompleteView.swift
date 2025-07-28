d03c08b753677dd558da516011a071e50cec8db7
김민석|Minseok Kim <alstjr7437@naver.com>
2025-07-28 21:54:41 +0900

    feat: 피드백 작성 완료 후 Hi-Fi 구현

--- GimiFeedback/GimiFeedback/Presentation/FeedbackWriteComplete/FeedbackWriteCompleteView.swift
+++ GimiFeedback/GimiFeedback/Presentation/FeedbackWriteComplete/FeedbackWriteCompleteView.swift
@@ -8,34 +8,43 @@
 import SwiftUI
 
 struct FeedbackWriteCompleteView: View {
-    @EnvironmentObject var mainRouter: MainNavigationRouter
-    @EnvironmentObject var onboardingRouter: OnboardingNavigationRouter
-    
+    let action: () -> Void
     let isLoggedIn: Bool = FirebaseAuthManager.currentUser
     
+    var buttonText: String {
+        isLoggedIn ? "홈으로 돌아가기": "처음으로 돌아가기"
+    }
+    
     var body: some View {
-        
         VStack {
-            Text("피드백 작성이 완료되었어요")
-            if isLoggedIn {
-                Button(action: {
-                    mainRouter.popToRootView()
-                }) {
-                    Text("홈으로 돌아가기")
-                }
-                .buttonStyle(.borderedProminent)
-            } else {
-                Button(action: {
-                    onboardingRouter.popToRootView()
-                }) {
-                    Text("로그인 하고 피드백 받기")
-                }
-                .buttonStyle(.borderedProminent)
+            Spacer()
+            
+            VStack(spacing: 16) {
+                Image(systemName: "checkmark")
+                    .resizable()
+                    .frame(width: 48, height: 48)
+                    .foregroundColor(.black)
+
+                Text("피드백 전송이 완료되었어요")
+                    .font(.title1)
+                    .foregroundColor(.black)
             }
+            .frame(maxWidth: .infinity)
+            
+            Spacer()
+            
+            Button(buttonText) {
+                action()
+            }
+            .buttonStyle(.gimiPrimary)
         }
+        .customPadding()
+        .ignoresSafeArea(edges: .top)
     }
 }
 
 #Preview {
-    FeedbackWriteCompleteView()
+    FeedbackWriteCompleteView {
+        print("Test")
+    }
 }
